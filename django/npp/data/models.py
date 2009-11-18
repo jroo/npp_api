adc803c0c7a46bc98a456f57f66e1473ebc1eae5
Joshua Ruihley <ruihley@gmail.com>
2009-11-18 19:04:59 +0000

    added model and import script for annual state energy expenditures

--- django/npp/data/models.py
+++ django/npp/data/models.py
@@ -6,6 +6,12 @@ class AnnualStateEnergyConsumption(models.Model):
     year = models.IntegerField()
     value = models.FloatField(null=True)
     
+class AnnualStateEnergyExpenditures(models.Model):
+    state = models.CharField(max_length=2)
+    msn = models.CharField(max_length=5)
+    year = models.IntegerField()
+    value = models.FloatField(null=True)
+    
 class MSNCodes(models.Model):
     msn = models.CharField(max_length=5)
     description = models.TextField()
