1285a2ea07b67c8073355b51f76afb6a07ea960e
Joshua Ruihley <ruihley@gmail.com>
2010-01-07 22:13:50 +0000

    Energy Consumption Stub

--- django/npp/api/handlers.py
+++ django/npp/api/handlers.py
@@ -1,11 +1,19 @@
-from piston.handler import BaseHandler
+from piston.handler import BaseHandler, AnonymousBaseHandler
 from data.models import AnnualStateEnergyConsumption
 
 class EnergyConsumptionHandler(BaseHandler):
     
     allowed_methods = ('GET',)
     model = AnnualStateEnergyConsumption
+    anonymous = 'AnonymousEnergyConsumptionHandler'
     
-    def read(self, request, id):
-        record = AnnualStateEnergyConsumption.objects.get(id=id)
-        return record
+    def read(self, request, id=None):
+        if id != None:
+            record = AnnualStateEnergyConsumption.objects.get(id=id)
+            return record
+        else:
+            records = AnnualStateEnergyConsumption.objects.all()[:10]
+            return records
+            
+class AnonymousEnergyConsumptionHandler(AnonymousBaseHandler):
+    fields = ('id')
--- django/npp/api/urls.py
+++ django/npp/api/urls.py
@@ -5,5 +5,10 @@ from npp.api.handlers import EnergyConsumptionHandler
 energy_consumption_handler = Resource(EnergyConsumptionHandler)
 
 urlpatterns = patterns('',
-   url(r'^energy_consumption/(?P<id>[^/]+)/', energy_consumption_handler),
+   url(r'^energy_consumption/$', energy_consumption_handler),
+   url(r'^energy_consumption/list\.(?P<emitter_format>.+)', energy_consumption_handler),
+   url(r'^energy_consumption/(?P<id>\d*)/$', energy_consumption_handler),
+
+   # automated documentation
+   #url(r'^$', documentation_view),
 )
