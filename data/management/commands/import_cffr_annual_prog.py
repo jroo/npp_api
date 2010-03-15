411ef41bb6d15d437954ceba3d25d7c510a3301d
Joshua Ruihley <ruihley@gmail.com>
2010-03-15 18:41:39 +0000

    merged pre93 and 93+ cffr program identification import scripts

--- data/management/commands/import_cffr_annual_prog.py
+++ data/management/commands/import_cffr_annual_prog.py
@@ -15,26 +15,32 @@ from data.models import CFFRProgram
 # HOWTO:
 # 1) Download .tar.gz from source data below
 # 2) decompress source data into a path and enter path into SOURCE_PATH var below
-# 3) Run as Django management command from your project path "python manage.py import_cffr_anual_pre93_prog"
+# 3) Run as Django management command from your project path "python manage.py import_cffr_anual_prog"
 # AFTER IMPORTING EVERY YEAR:
 # 4) Create indexes in database
-#   CREATE INDEX idx_year ON data_cffrprog (year)
+#   CREATE INDEX idx_year ON data_cffrprogram (year)
 
-YEAR = 1982
+YEAR = 1983
 SOURCE_PATH = '%s/cffr/%s/' % (settings.LOCAL_DATA_ROOT, YEAR)
-SOURCE_FILE = '%sPROG%s.DAT' % (SOURCE_PATH, str(YEAR))
+
+if YEAR < 1993:
+    SOURCE_FILE = '%sPROG%s.DAT' % (SOURCE_PATH, str(YEAR))
+else:
+    SOURCE_FILE = '%s%sprog.txt' % (SOURCE_PATH, str(YEAR)[2:4])    
 
 class Command(NoArgsCommand):
     
     def handle_noargs(self, **options):
+        print SOURCE_FILE
         f = open(SOURCE_FILE, 'r')
 
         for line in f:
             program_id_code= line[0:6]
             program_name = line[6:79]
             
+            print (YEAR, program_id_code, program_name)
             record = CFFRProgram(year=YEAR, program_id_code=program_id_code, program_name=program_name)
-
+        
             try:
                 record.save()
                 db.reset_queries()
