930f1578de3345d10a6e9e9bc31822768d147b66
Joshua Ruihley <ruihley@gmail.com>
2010-04-29 21:05:25 +0000

    finish irs import, create vehicle registrations model

--- data/management/commands/import_irs_gross.py
+++ data/management/commands/import_irs_gross.py
@@ -16,30 +16,40 @@ import csv
 # HOWTO:
 # 1) Download source files from url listed above
 # 2) change SOURCE_FILE variable to the the path of the source file you just downloaded
-# 3) Run as Django management command from your project path "python manage.py import_irs_gross
+# 3) change column (row[]) values below to reflect where column exists in csv
+# 4) Run as Django management command from your project path "python manage.py import_irs_gross
+# 5) Add index to year column in db
 
-YEAR = 2009
+YEAR = 2007
 SOURCE_PATH = '%s/irs/' % (settings.LOCAL_DATA_ROOT)
 SOURCE_FILE = '%s%sIRS.csv' % (SOURCE_PATH, str(YEAR))
 
 class Command(NoArgsCommand):
     
     def handle_noargs(self, **options):
+    
+        def clean_value(value):
+            value = value.replace(' ', '')
+            if value <> '':
+                value = int(value.replace(',', ''))
+            else:
+                value = None
+            return value
+    
         data_reader = csv.reader(open(SOURCE_FILE))
         for row in data_reader:
             state = row[0]
-            print state
-            total_collections = int(row[1].replace(',', ''))
-            business_income_taxes = int(row[2].replace(',', ''))
-            individual_total = int(row[3].replace(',', ''))
-            individual_witheld_fica = int(row[4].replace(',', ''))
-            individual_notwitheld_seca = int(row[5].replace(',', ''))
-            individual_unemployment = int(row[6].replace(',', ''))
-            individual_railroad_retirement = int(row[7].replace(',', ''))
-            individual_estate_trust_income = int(row[8].replace(',', ''))
-            estate_tax = int(row[9].replace(',', ''))
-            gift_tax = int(row[10].replace(',', ''))
-            excise_taxes = int(row[11].replace(',', ''))
+            total_collections = clean_value(row[1])
+            business_income_taxes = clean_value(row[2])
+            individual_total = clean_value(row[3])
+            individual_witheld_fica = clean_value(row[5])
+            individual_notwitheld_seca = clean_value(row[4])
+            individual_unemployment = clean_value(row[7])
+            individual_railroad_retirement = clean_value(row[6])
+            individual_estate_trust_income = None
+            estate_tax = clean_value(row[8])
+            gift_tax = clean_value(row[9])
+            excise_taxes = clean_value(row[10])
             
             record = IRSGrossCollections(year=YEAR, state=state, total_collections=total_collections,
                 business_income_taxes=business_income_taxes,
@@ -50,3 +60,4 @@ class Command(NoArgsCommand):
                 individual_estate_trust_income=individual_estate_trust_income,
                 estate_tax = estate_tax, gift_tax=gift_tax, excise_taxes=excise_taxes)
             record.save()
+            print state
